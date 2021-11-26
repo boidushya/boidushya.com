c4b4193b1aaaedcf9baa34c31f6bb7c76bfa74e1
boidushya <boidushyabhattacharya@gmail.com>
2021-11-26 17:38:01 +0530

    fix: shifted images to local

--- src/elements/Dock/DockContent.jsx
+++ src/elements/Dock/DockContent.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import "@styles/dock.scss";
+import FinderIcon from "@static/finder.png";
+import CodeIcon from "@static/code.png";
+import GithubIcon from "@static/github.png";
+import QemuIcon from "@static/qemu.png";
+import FolderIcon from "@static/folder.png";
 import { Link } from "react-router-dom";
 
 const DockContent = () => {
@@ -12,28 +17,28 @@ const DockContent = () => {
 						<ul>
 							<Link to="/">
 								<li data-title="Home" className="full-width-icon">
-									<img src="https://support.apple.com/library/content/dam/edam/applecare/images/en_US/macos/Big-Sur/macos-big-sur-finder-icon.png" className="img-fluid" alt="mac"/>
+									<img src={FinderIcon} className="img-fluid" alt="mac"/>
 								</li>
 							</Link>
 							<Link to="/vscode">
 								<li data-title="VS Code">
-									<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/2048px-Visual_Studio_Code_1.35_icon.svg.png" className="img-fluid" alt="mac"/>
+									<img src={CodeIcon} className="img-fluid" alt="mac"/>
 								</li>
 							</Link>
 							<Link to="/git">
 								<li data-title="Git Log">
-									<img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" className="img-fluid" alt="mac" />
+									<img src={GithubIcon} className="img-fluid" alt="mac" />
 								</li>
 							</Link>
 							<Link to="/qemu">
 								<li data-title="Qemu">
-									<img src="https://icons-for-free.com/iconfiles/png/512/qemu-1324440219327920651.png" className="img-fluid" alt="mac" />
+									<img src={QemuIcon} className="img-fluid" alt="mac" />
 								</li>
 							</Link>
 							<div className="separator"/>
 							<Link to="/resume">
 								<li data-title="Resume">
-									<img src="https://cdn.icon-icons.com/icons2/2963/PNG/512/macos_big_sur_folder_icon_186046.png" className="img-fluid" alt="mac" />
+									<img src={FolderIcon} className="img-fluid" alt="mac" />
 								</li>
 							</Link>
 						</ul>
