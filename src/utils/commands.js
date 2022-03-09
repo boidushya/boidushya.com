d53abc071337525972198e25c8078b4dc9cc8dde
boidushya <boidushyabhattacharya@gmail.com>
2022-03-09 19:06:28 +0530

    feat: added projects section and WIP warning

--- src/utils/commands.js
+++ src/utils/commands.js
@@ -1,240 +1,274 @@
 import neofetch from "@utils/neofetch";
 //eslint-disable-next-line
 
-const compileResponseHTML = (styleMap) => {
-	return styleMap.map((item)=>{
-		return `<span class="${item.folder?`style3`:`${item.executable?`style2`:`style1`}`}">${item.link?`<a target="_blank" href="${item.link}">${item.name}</a>`:`${item.name}`}</span>`
-	}).join("  ")
-}
+const compileResponseHTML = styleMap => {
+	return styleMap
+		.map(item => {
+			return `<span class="${
+				item.folder
+					? `style3`
+					: `${item.executable ? `style2` : `style1`}`
+			}">${
+				item.link
+					? `<a target="_blank" href="${item.link}">${item.name}</a>`
+					: `${item.name}`
+			}</span>`;
+		})
+		.join("  ");
+};
 
-const getSpaces = (commandList) => {
-	let defaultSpaces = "\t"
+const getSpaces = commandList => {
+	let defaultSpaces = "\t";
 	let lengthList = commandList.map(item => {
-		return item.name.length
-	})
+		return item.name.length;
+	});
 	// console.log(commandList)
-	let max = Math.max(...lengthList)
+	let max = Math.max(...lengthList);
 	let what = commandList.map(item => {
-		return Array(max - item.name.length + 1).join(" ") + defaultSpaces
-	})
-	return what
-}
+		return Array(max - item.name.length + 1).join(" ") + defaultSpaces;
+	});
+	return what;
+};
 
-const compileCommandHTML = (commandList) => {
+const compileCommandHTML = commandList => {
 	let defArgs = [
 		{
-			"name": "ls",
-			"description": "lists directory content"
+			name: "ls",
+			description: "lists directory content",
 		},
 		{
-			"name": "cd",
-			"description": "changes the current working directory"
+			name: "cd",
+			description: "changes the current working directory",
 		},
 		{
-			"name": "clear",
-			"description": "clears the terminal screen"
-		}
-	]
-	let argList = [...defArgs, ...commandList.map(item => {
-		let extra = " "
-		if(item.subPathStrict[0]){
-			extra += item.subPathStrict[1].name
-		}
-		return {
-			name: item.name[0]+extra,
-			description: item.description
-		}
-	})]
-	let spaceList = getSpaces(argList)
+			name: "clear",
+			description: "clears the terminal screen",
+		},
+	];
+	let argList = [
+		...defArgs,
+		...commandList.map(item => {
+			let extra = " ";
+			if (item.subPathStrict[0]) {
+				extra += item.subPathStrict[1].name;
+			}
+			return {
+				name: item.name[0] + extra,
+				description: item.description,
+			};
+		}),
+	];
+	let spaceList = getSpaces(argList);
 	let response = `ZSH version 5.8.0 (x86_64-apple-darwin20.0)
 These shell commands are defined internally.
-Type <span class="style2">'help'</span> to see this list.\n\n`
+Type <span class="style2">'help'</span> to see this list.\n\n`;
 	argList.forEach((item, idx) => {
-		let temp = `<span class="style2">${item.name}</span>${spaceList[idx]}${item.description}\n`
-		response += temp
-	})
-	return `${response}\nAnd more "hidden" commands...`
-}
+		let temp = `<span class="style2">${item.name}</span>${spaceList[idx]}${item.description}\n`;
+		response += temp;
+	});
+	return `${response}\nAnd more "hidden" commands...`;
+};
 
 let commandList = [
 	{
-		"name":["resume","./resume"],
-		"action": { "RESUME":""},
-		"response":"",
-		"subPathStrict":[false],
-		"description":"view my resume"
+		name: ["resume", "./resume", "resume.sh", "./resume.sh"],
+		action: { RESUME: "" },
+		response: "",
+		subPathStrict: [false],
+		description: "view my resume",
 	},
 	{
-		"name": ["neofetch"],
-		"action": false,
-		"response": `<pre>${neofetch}</pre>`,
-		"subPathStrict": [false],
-		"description": "displays information about me in an aesthetic and visually pleasing way."
+		name: ["projects", "./projects", "projects.app", "./projects.app"],
+		// action: { PROJECTS: "" },
+		action: false,
+		response: "Under construction 🚧🔨",
+		subPathStrict: [false],
+		description: "checkout my projects",
 	},
 	{
-		"name": ["code"],
-		"action": true,
-		"response": "",
-		"subPathStrict": [true, {"name":".","response":""}],
-		"description": "opens a VS code window for this website's source code"
+		name: ["neofetch"],
+		action: false,
+		response: `<pre>${neofetch}</pre>`,
+		subPathStrict: [false],
+		description:
+			"displays information about me in an aesthetic and visually pleasing way.",
 	},
 	{
-		"name": ["danger"],
-		"action": true,
-		"response": "",
-		"subPathStrict": [false],
-		"description": "<span class=\"style7\">¯\\_(ツ)_/¯</span>"
+		name: ["code"],
+		action: true,
+		response: "",
+		subPathStrict: [true, { name: ".", response: "" }],
+		description: "opens a VS code window for this website's source code",
 	},
 	{
-		"name": ["git"],
-		"action": true,
-		"response": "",
-		"subPathStrict": [true,{"name":"log","response":""}],
-		"description": "lists my github projects"
+		name: ["danger"],
+		action: true,
+		response: "",
+		subPathStrict: [false],
+		description: '<span class="style7">¯\\_(ツ)_/¯</span>',
 	},
 	{
-		"name": ["qemu"],
-		"action": true,
-		"response": "",
-		"subPathStrict": [false],
-		"description": "a linux emulator that runs right on your browser (I had to flex I'm sorry)"
+		name: ["git"],
+		action: true,
+		response: "",
+		subPathStrict: [true, { name: "log", response: "" }],
+		description: "lists my github projects",
 	},
 	{
-		"name": ["help"],
-		"action": true,
-		"response": "",
-		"subPathStrict": [false],
-		"description": "displays this message "
+		name: ["qemu"],
+		action: true,
+		response: "",
+		subPathStrict: [false],
+		description:
+			"a linux emulator that runs right on your browser (I had to flex I'm sorry)",
 	},
 	{
-		"name": ["uname"],
-		"action": true,
-		"response": "Darwin MacBook-Pro.local 20.1.0 Darwin Kernel Version 20.1.0: Sun Jul 18 10:32:00 PDT 2021; root:xnu-7195.50.7~2/RELEASE_ARM64_T8101 arm64",
-		"subPathStrict": [false],
-		"description": "prints the Darwin OS Kernel version / release / machine name. "
+		name: ["help"],
+		action: true,
+		response: "",
+		subPathStrict: [false],
+		description: "displays this message ",
 	},
 	{
-		"name": ["whoami"],
-		"action": true,
-		"response": "Boidushya Bhattacharyay",
-		"subPathStrict": [false],
-		"description": "prints the full name of the current administrator when invoked"
-	}
-]
+		name: ["uname"],
+		action: true,
+		response:
+			"Darwin MacBook-Pro.local 20.1.0 Darwin Kernel Version 20.1.0: Sun Jul 18 10:32:00 PDT 2021; root:xnu-7195.50.7~2/RELEASE_ARM64_T8101 arm64",
+		subPathStrict: [false],
+		description:
+			"prints the Darwin OS Kernel version / release / machine name. ",
+	},
+	{
+		name: ["whoami"],
+		action: true,
+		response: "Boidushya Bhattacharyay",
+		subPathStrict: [false],
+		description:
+			"prints the full name of the current administrator when invoked",
+	},
+];
 
 commandList = commandList.map(item => {
 	if (item.name[0] === "help") {
-		item.response = `<pre>${compileCommandHTML(commandList)}</pre>`
+		item.response = `<pre>${compileCommandHTML(commandList)}</pre>`;
 	}
 	return item;
-})
+});
 
 const fileList = [
 	{
-		name:".github",
-		link:"https://github.com/boidushya",
-		folder:true,
-		executable:false,
+		name: ".github",
+		link: "https://github.com/boidushya",
+		folder: true,
+		executable: false,
 	},
 	{
-		name:"src",
-		link:"https://github.com/boidushya/boidushya.com",
-		folder:true,
-		executable:false,
+		name: "src",
+		link: "https://github.com/boidushya/boidushya.com",
+		folder: true,
+		executable: false,
 	},
 	{
-		name:"resume",
-		link:"",
-		folder:false,
-		executable:true,
+		name: "resume.sh",
+		link: "",
+		folder: false,
+		executable: true,
 	},
-]
+	{
+		name: "projects.app",
+		link: "",
+		folder: false,
+		executable: true,
+	},
+];
 
-const getCommandList = (commandList) => {
-	let finalCommandList = {}
+const getCommandList = commandList => {
+	let finalCommandList = {};
 	commandList.forEach(item => {
 		//eslint-disable-next-line
-		let commandBuilder = {}
+		let commandBuilder = {};
 		item.name.forEach(elem => {
-			let action = item.action ? { [item.name[0].toUpperCase()]: "" } : null
+			let action = item.action
+				? { [item.name[0].toUpperCase()]: "" }
+				: null;
 			let resp = item.response;
 			commandBuilder = {
 				[elem]: {
-					"validArgs": {
-						"_dir": {
+					validArgs: {
+						_dir: {
 							action: action,
 							response: resp,
 						},
-						"default": {
+						default: {
 							action: action,
 							response: resp,
-						}
-					}
-				}
-			}
+						},
+					},
+				},
+			};
 			if (item.subPathStrict[0]) {
 				commandBuilder[elem].validArgs[item.subPathStrict[1].name] = {
 					action: action,
-					response: item.subPathStrict[1].response
-				}
+					response: item.subPathStrict[1].response,
+				};
 			}
-			finalCommandList = { ...commandBuilder, ...finalCommandList }
-		})
-	})
+			finalCommandList = { ...commandBuilder, ...finalCommandList };
+		});
+	});
 	// console.log(finalCommandList)
-	return finalCommandList
-}
+	return finalCommandList;
+};
 
-const getArgListCd = (fileList) => {
+const getArgListCd = fileList => {
 	let defArgs = {
-		"_dir": {
-			"action": null,
-			"response": ""
+		_dir: {
+			action: null,
+			response: "",
 		},
-		"default": {
-			"action": null,
-			"response": "cd: cannot access %arg%: Permission Denied"
+		default: {
+			action: null,
+			response: "cd: cannot access %arg%: Permission Denied",
 		},
 		"~": {
-			"action": null,
-			"response": ""
-		}
-	}
-	let argList = {}
-	fileList.forEach((item)=>{
+			action: null,
+			response: "",
+		},
+	};
+	let argList = {};
+	fileList.forEach(item => {
 		argList[item.name] = {
-			action:item.folder?{PATH:item.link}:null,
-			response:item.folder?"":"zsh: cd: %arg%: Not a directory",
-		}
-	})
-	Object.keys(defArgs).forEach((item)=>{
-		argList[item] = defArgs[item]
-	})
-	return argList
-}
+			action: item.folder ? { PATH: item.link } : null,
+			response: item.folder ? "" : "zsh: cd: %arg%: Not a directory",
+		};
+	});
+	Object.keys(defArgs).forEach(item => {
+		argList[item] = defArgs[item];
+	});
+	return argList;
+};
 
 const commands = {
-	"ls": {
-		"validArgs":{
-			"/":{
-				"action":null,
-				"response": "ls: cannot access System Volume Information: Permission Denied"
+	ls: {
+		validArgs: {
+			"/": {
+				action: null,
+				response:
+					"ls: cannot access System Volume Information: Permission Denied",
 			},
-			"_dir":{
-				"action":null,
-				"response":compileResponseHTML(fileList)
+			_dir: {
+				action: null,
+				response: compileResponseHTML(fileList),
 			},
-			"default":{
-				"action":null,
-				"response":"ls: cannot access %arg%: Permission Denied"
-			}
-		}
+			default: {
+				action: null,
+				response: "ls: cannot access %arg%: Permission Denied",
+			},
+		},
 	},
-	"cd": {
-		"validArgs": getArgListCd(fileList)
+	cd: {
+		validArgs: getArgListCd(fileList),
 	},
-	...getCommandList(commandList)
-}
+	...getCommandList(commandList),
+};
 
-
-export default commands
+export default commands;
--- src/utils/responseFetcher.js
+++ src/utils/responseFetcher.js
@@ -1,80 +1,90 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect } from "react";
 import commands from "@utils/commands";
 import styled from "styled-components";
 import theme from "@styles/theme";
 import { sanitize } from "dompurify";
-import DataContext from '@contexts/Data/DataContext';
+import DataContext from "@contexts/Data/DataContext";
 
 // eslint-disable-next-line
 const Wrapper = styled.span`
-	a{
-		color:inherit;
-		background:inherit;
-		text-decoration:none;
+	a {
+		color: inherit;
+		background: inherit;
+		text-decoration: none;
 	}
-	.style1{
+	.style1 {
 		color: ${theme.bodyFont1};
 	}
-	.style2{
+	.style2 {
 		color: ${theme.bodyFont2};
 	}
-	.style3{
+	.style3 {
 		color: ${theme.bodyFont2.darken(0.75)};
 		background: ${theme.bodyFont1};
 	}
-	.style4{
+	.style4 {
 		color: ${theme.bodyFont3};
 	}
-	.style5{
+	.style5 {
 		color: ${theme.bodyFont4};
 	}
-	.style6{
+	.style6 {
 		color: ${theme.bodyFont5};
 	}
-	.style7{
+	.style7 {
 		background: ${theme.easterEgg};
 		background-size: 1800% 1800%;
 		animation: rainbow 15s ease infinite;
 
 		@keyframes rainbow {
-			0%{background-position:0% 82%}
-			50%{background-position:100% 19%}
-			100%{background-position:0% 82%}
+			0% {
+				background-position: 0% 82%;
+			}
+			50% {
+				background-position: 100% 19%;
+			}
+			100% {
+				background-position: 0% 82%;
+			}
 		}
 		color: ${theme.bodyFont4};
 	}
-`
+`;
 
-const getErrorCommand = (command) => {
+const getErrorCommand = command => {
 	return (
 		<Wrapper>
-			zsh: command not found: <span className="style2">{sanitize(command)}</span>
+			zsh: command not found:{" "}
+			<span className="style2">{sanitize(command)}</span>
 		</Wrapper>
-	)
-}
+	);
+};
 
-const Action = (props) => {
-	const { setAlertHidden } = useContext(DataContext)
+const Action = props => {
+	const { setAlertHidden } = useContext(DataContext);
 	useEffect(() => {
-		if(props.action){
-			switch(Object.keys(props.action)[0]){
+		if (props.action) {
+			switch (Object.keys(props.action)[0]) {
 				case "PATH":
-					window.open(props.action.PATH)
+					window.open(props.action.PATH);
 					break;
 				case "CODE":
-					document.location.href = "/vscode"
+					document.location.href = "/vscode";
 					break;
 				case "DANGER":
-					document.location.href = "/danger-zone"
+					document.location.href = "/danger-zone";
 					break;
 				case "QEMU":
-					document.location.href = "/qemu"
+					document.location.href = "/qemu";
 					break;
 				case "RESUME":
-					document.location.href = "/resume"
+					document.location.href = "/resume";
+					break;
+				case "PROJECTS":
+					document.location.href = "/projects";
 					break;
 				case "GIT":
-					document.location.href = "/git"
+					document.location.href = "/git";
 					break;
 				case "HELP":
 					setAlertHidden(true);
@@ -82,52 +92,55 @@ const Action = (props) => {
 				default:
 					break;
 			}
-
 		}
-	//eslint-disable-next-line
-	}, [])
-	return (
-		<></>
-	)
-}
+		//eslint-disable-next-line
+	}, []);
+	return <></>;
+};
 
-const parseResponse = (command,{data}) => {
-	let response = command.response
+const parseResponse = (command, { data }) => {
+	let response = command.response;
 	return (
 		<>
-			<Action action={command.action}/>
+			<Action action={command.action} />
 			<Wrapper
 				dangerouslySetInnerHTML={{
-					__html:response.replace(/%arg%/g,`<span class="style2">${sanitize(data)}</span>`)
+					__html: response.replace(
+						/%arg%/g,
+						`<span class="style2">${sanitize(data)}</span>`
+					),
 				}}
 			/>
 		</>
-	)
-}
+	);
+};
 
-const getResponse = (data) => {
-	let commandWithArgs = data.split(" ")
-	if(commands.hasOwnProperty(commandWithArgs[0])){
-		let arg = ""
-		let originalArg = ""
-		if(commandWithArgs.length===1){
-			arg = "_dir"
+const getResponse = data => {
+	let commandWithArgs = data.split(" ");
+	if (commands.hasOwnProperty(commandWithArgs[0])) {
+		let arg = "";
+		let originalArg = "";
+		if (commandWithArgs.length === 1) {
+			arg = "_dir";
+		} else if (
+			commands[commandWithArgs[0]].validArgs.hasOwnProperty(
+				commandWithArgs[1]
+			)
+		) {
+			arg = commandWithArgs[1];
+		} else {
+			arg = "default";
 		}
-		else if(commands[commandWithArgs[0]].validArgs.hasOwnProperty(commandWithArgs[1])){
-			arg = commandWithArgs[1]
+		originalArg = arg;
+		if (arg === "default") {
+			originalArg = commandWithArgs[1];
 		}
-		else{
-			arg = "default"
-		}
-		originalArg = arg
-		if(arg==="default"){
-			originalArg = commandWithArgs[1]
-		}
-		return parseResponse(commands[commandWithArgs[0]].validArgs[arg],{data:originalArg})
+		return parseResponse(commands[commandWithArgs[0]].validArgs[arg], {
+			data: originalArg,
+		});
+	} else {
+		return getErrorCommand(data);
 	}
-	else{
-		return getErrorCommand(data)
-	}
-}
+};
 
-export default getResponse
+export default getResponse;
