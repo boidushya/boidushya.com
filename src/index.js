1832d04783e4327b8d31a43cbf32d911f8ba2d85
Boidushya Bhattacharya <boidushyabhattacharya@gmail.com>
2023-02-05 23:59:06 +0530

    fix: reverted to using render instead of createRoot

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import ReactGA from "react-ga";
 import "@styles/index.scss";
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 ReactGA.initialize("UA-170076691-3");
 ReactGA.pageview(window.location.pathname + window.location.search);
